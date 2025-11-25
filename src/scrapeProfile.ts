598c5f643d7e7057e00d839a1afedbf2584cc1ab
xorcuit <xorcuit@gmail.com>
2025-11-26 05:39:12 +0700

    fix: lru cache size calc

--- src/scrapeProfile.ts
+++ src/scrapeProfile.ts
@@ -3,7 +3,12 @@ import { createConcurrentQueuesPerKey } from './utils/queue.js';
 import { LRUCache } from 'lru-cache';
 import { Actor, ActorPricingInfo } from 'apify';
 
-const profileCache = new LRUCache({ maxSize: 500 });
+const profileCache = new LRUCache({
+  maxSize: 500,
+  sizeCalculation: () => {
+    return 1;
+  },
+});
 
 export const scrapeProfile = createConcurrentQueuesPerKey(
   (args) => args.linkedinUrl,
@@ -19,25 +24,25 @@ export const scrapeProfile = createConcurrentQueuesPerKey(
   }): Promise<{ profile: Profile | null }> => {
     let profile: Profile | null = null;
 
-    if (profileCache.has(linkedinUrl)) {
-      profile = profileCache.get(linkedinUrl) as Profile;
-    } else {
-      const result = await scraper
-        .getProfile({
+    try {
+      if (profileCache.has(linkedinUrl)) {
+        profile = profileCache.get(linkedinUrl) as Profile;
+      } else {
+        const result = await scraper.getProfile({
           url: linkedinUrl,
           short: true,
-        })
-        .catch((err) => {
-          console.warn(`Failed to fetch profile ${linkedinUrl}: ${err.message}`);
-          return null;
         });
-      if (result?.element?.id) {
-        if (pricingInfo.isPayPerEvent) {
-          Actor.charge({ eventName: 'main-profile' });
+
+        if (result?.element?.id) {
+          if (pricingInfo.isPayPerEvent) {
+            Actor.charge({ eventName: 'main-profile' });
+          }
+          profileCache.set(linkedinUrl, result.element);
+          profile = result.element;
         }
-        profileCache.set(linkedinUrl, result.element);
-        profile = result.element;
       }
+    } catch (error) {
+      console.error(`Failed to scrape profile at ${linkedinUrl}: ${(error as Error).message}`);
     }
 
     return { profile };
